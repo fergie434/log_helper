aaef57ef5e116c06a1b3bf057fa71a4da22b27c2
Simon Ferguson <simon.ferguson@education.wa.edu.au>
2025-06-26 16:45:37 +0800

    Customizable log level

--- log_helper/main.py
+++ log_helper/main.py
@@ -5,14 +5,14 @@ import os
 import sys
 from pathlib import Path
 
-def setup_logging(log_filename='log.log', log_maxbytes=4000000, log_filecount=5, log_directory='logs'):
+def setup_logging(log_filename='log.log', log_maxbytes=4000000, log_filecount=5, log_directory='logs', log_level=logging.INFO):
     log_filepath = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), log_directory)
     log_filename = os.path.join(log_filepath, log_filename)
 
     Path(log_filepath).mkdir(exist_ok=True)
 
     logger = logging.getLogger(__name__)
-    logger.setLevel(level=logging.INFO)
+    logger.setLevel(level=log_level)
     formatter = logging.Formatter(
             '%(asctime)s - %(levelname)s - %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S'
